baa5b8a53e34b303a1acf68c6622435244f11888
Joaco Del Aguila <joacodelaguila@gmail.com>
2024-06-12 19:47:50 -0300

    clase 20

--- inicio/models.py
+++ inicio/models.py
@@ -5,3 +5,5 @@ class Perro(models.Model):
     raza = models.CharField(max_length=20)
     edad = models.IntegerField()
     
+    def __str__(self):
+        return f"{self.nombre} {self.raza} {self.edad}"
--- inicio/urls.py
+++ inicio/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from inicio import views
 
 urlpatterns = [
-    path("", views.inicio),
+    path("", views.inicio, name="inicio"),
     path("template1/<str:nombre>/<str:apellido>/<int:edad>", views.template1),
     path("template2/<str:nombre>/<str:apellido>/<int:edad>", views.template2),
     path("template3/<str:nombre>/<str:apellido>/<int:edad>", views.template3),
     path("template4/<str:nombre>/<str:apellido>/<int:edad>", views.template4),
-    path("probando/", views.probando),
-    path("perros/crear/<str:nombre>/<str:raza>/<int:edad>/", views.crear_perro)
+    path("probando/", views.probando, name="probando"),
+    path("perros/crear/<str:nombre>/<str:raza>/<int:edad>/", views.crear_perro, name= "crear_perro")
     ]
     
